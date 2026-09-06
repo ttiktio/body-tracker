85e27b67a9a9033c0f7f4927d9afb5c418de5ff5
Poramate S <132546784+ttiktio@users.noreply.github.com>
2026-09-06 14:06:32 +0700

    Cache V3 enhancement script for offline use

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='body-tracker-v3';
-const ASSETS=['./','./index.html','./style.css','./app.js','./body3d.js','./manifest.json','./icon-192.svg','./icon-512.svg'];
+const ASSETS=['./','./index.html','./style.css','./app.js','./body3d.js','./units.js','./manifest.json','./icon-192.svg','./icon-512.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',event=>{
